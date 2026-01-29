11cb1f54c1f51c7d4d647baff5e7dda9bbcc6f6e
csrepo34-debug <csrepo34@gmail.com>
2026-01-29 10:54:41 +0530

    added banners to prod lang prods2

--- frontend/app/products/programming-languages/page.js
+++ frontend/app/products/programming-languages/page.js
@@ -71,8 +71,6 @@ export default function ProgrammingLanguagesPage() {
               </a>
             </article>
           );
-            </a>
-          </article>
         ))}
       </div>
     </div>
